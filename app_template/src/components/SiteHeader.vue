031526f939fd5b782d85999e338f1b8947fb25aa
mutsuyuki <muttuwo.mob@gmail.com>
2020-03-06 13:39:03 +0900

    サービスタイトルを外から渡せるように

--- app_template/src/components/SiteHeader.vue
+++ app_template/src/components/SiteHeader.vue
@@ -5,7 +5,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                     <path fill="#555" d="M0 0v24h24v-24h-24zm18 18h-12v-12h12v12z"/>
                 </svg>
-                <span class="title"> MY SERVICE </span>
+                <span class="title"> {{title}} </span>
             </div>
         </router-link>
 
@@ -26,7 +26,7 @@
 
 
 <script lang="ts">
-    import {Component, Vue} from 'vue-property-decorator';
+    import {Component, Prop, Vue} from 'vue-property-decorator';
     import UserStore from "../../../UserStore/UserStore";
     import UserMenu from './UserMenu.vue';
 
@@ -35,6 +35,8 @@
     })
 
     export default class SiteHeader extends Vue {
+
+        @Prop({default: 'MY SERVICE'}) private title!: string;
 
         private isOpenMenu: boolean = false;
 
