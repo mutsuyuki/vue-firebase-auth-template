f5ef33d95d13ffb0b25a8e6c38e0b51d056155bd
mutsuyuki <muttuwo.mob@gmail.com>
2020-08-16 23:21:50 +0900

    Update DeepCloner.ts

--- src/store/common/DeepCloner.ts
+++ src/store/common/DeepCloner.ts
@@ -1,7 +1,36 @@
 export default class DeepCloner {
 
-    public static copyJson<T>(source: T): T {
-        return JSON.parse(JSON.stringify(source));
+    public static copyJson<T>(target: T): T {
+        if (target === null) {
+          return target
+        }
+        if (target instanceof Date) {
+          return new Date(target.getTime()) as any
+        }
+        // First part is for array and second part is for Realm.Collection
+        // if (target instanceof Array || typeof (target as any).type === 'string') {
+        if (typeof target === 'object') {
+          if (typeof target[(Symbol as any).iterator] === 'function') {
+            const cp = [] as any[]
+            if ((target as any as any[]).length > 0) {
+              for (const arrayMember of target as any as any[]) {
+                cp.push(DeepCloner.copyJson(arrayMember))
+              }
+            }
+            return cp as any as T
+          } else {
+            const targetKeys = Object.keys(target)
+            const cp = {}
+            if (targetKeys.length > 0) {
+              for (const key of targetKeys) {
+                cp[key] = DeepCloner.copyJson(target[key])
+              }
+            }
+            return cp as T
+          }
+        }
+        // Means that object is atomic
+        return target
     }
 
 }
